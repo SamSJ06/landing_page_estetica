f4f33db5fb3821e33bf7d8d93bddad7b1e200b4e
Bialima16 <beatrizlima2738@gmail.com>
2024-06-15 18:41:57 -0300

    Adição do hamburgue

---
+++ src/script/script.js
@@ -0,0 +1,15 @@
+function abrirMenu() {
+    const menu = document.querySelector("aside.sidebar");
+    const Button = document.getElementById("checkbox-menu");
+    if (menu.classList.contains("barfechar")) {
+     menu.classList.remove("barfechar");
+     menu.classList.add("menuabe");
+     Button.classList.remove("checkbox-menu");
+     Button.classList.add("abertobo");
+    } else {
+      menu.classList.remove("menuabe");
+      menu.classList.add("barfechar");
+      Button.classList.remove("abertobo");
+     Button.classList.add("checkbox-menu");
+    }
+  }
